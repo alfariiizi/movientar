e881880952334dd503e1b9099e39e051cc7889bb
alfariiizi <alfariiiziiiii@gmail.com>
2023-02-24 19:29:25 +0700

    Centering dropdown in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ import { tvsData, moviesData } from "../data";
 const Navbar = ({ titleSite }) => {
   return (
     <nav className="z-[100drop down menu on navbar react] relative">
-      <div className="fixed flex h-14 w-full items-center justify-between border-b-[1px] border-b-slate-900 bg-slate-100/30 px-4 backdrop-blur-md">
-        <h1 className="text-xl">{titleSite}</h1>
-        <div className="flex w-full items-center justify-around">
+      <div className="fixed flex h-14 w-full items-center justify-between gap-2 border-b-[1px] border-b-slate-900 bg-slate-100/30 px-4 backdrop-blur-md">
+        <h1 className="flex-none text-center text-xl sm:w-1/3">{titleSite}</h1>
+        <div className="flex grow items-center justify-center gap-4">
           <Dropdown
             name={"Movies"}
             options={[moviesData.popular, moviesData.topRated]}
