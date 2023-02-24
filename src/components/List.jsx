3a2f9ce7803f3769cc6958b5a6af896259bec2a8
alfariiizi <alfariiiziiiii@gmail.com>
2023-02-24 19:49:46 +0700

    Adding retro backside of every card

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -13,23 +13,25 @@ const List = ({ kind }) => {
       {/* List of all cards */}
       <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-4">
         {getList.map((movie, index) => (
-          <div
-            className="flex h-80 w-40 flex-col border-2 border-slate-800"
-            key={index}>
-            {/* Card image */}
-            <div className="h-60 w-full bg-blue-400">
-              <img
-                className="h-full w-full"
-                src={`${process.env.REACT_APP_BASEIMAGEURL}${movie.poster_path}`}
-                alt=""
-              />
+          <div key={index} className="group relative h-80 w-40 transition-all">
+            <div className="relative z-10 flex h-full w-full flex-col border-2 border-slate-800 bg-slate-200">
+              {/* Card image */}
+              <div className="h-60 w-full bg-blue-400">
+                <img
+                  className="h-full w-full"
+                  src={`${process.env.REACT_APP_BASEIMAGEURL}${movie.poster_path}`}
+                  alt=""
+                />
+              </div>
+              {/* Card description */}
+              <div className="flex flex-col items-center justify-between px-2 text-sm">
+                <h2 className="font-bold">{movie.title}</h2>
+                <p className="">Rating: {movie.vote_average}</p>
+                <p>Release: {movie.release_date}</p>
+              </div>
             </div>
-            {/* Card description */}
-            <div className="flex flex-col items-center justify-between px-2 text-sm">
-              <h2 className="font-bold">{movie.title}</h2>
-              <p className="">Rating: {movie.vote_average}</p>
-              <p>Release: {movie.release_date}</p>
-            </div>
+            {/* Backside retro card style */}
+            <div className="absolute top-2 left-2 hidden h-full w-full border-2 border-slate-800 bg-teal-500 transition-all group-hover:block"></div>
           </div>
         ))}
       </div>
