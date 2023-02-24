7b5008365ea80adb4712909da102e883b27569b8
alfariiizi <alfariiiziiiii@gmail.com>
2023-02-24 20:58:08 +0700

    Adding arrow in dropdown navbar

--- src/tools/Dropdown.jsx
+++ src/tools/Dropdown.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Dropdown = ({ name, options }) => {
   return (
-    <button className="group relative h-full border-2 border-slate-800 bg-gray-200 p-2">
+    <button className="group relative flex h-full items-center justify-between border-2 border-slate-800 bg-gray-200 p-2">
       {name}
-      <div className="absolute top-10 left-0 z-[110] hidden h-auto w-auto border-2 border-slate-800 bg-gray-200 group-hover:flex group-hover:flex-col group-hover:gap-1">
+      <RiArrowDropDownLine />
+      <div className="absolute top-10 left-0 z-[110] hidden h-auto w-auto flex-col gap-1 border-2 border-slate-800 bg-gray-200 duration-100 group-hover:flex">
         {options.map((option, index) => (
-          <div key={index} className="h-full w-full p-1 hover:bg-gray-300">
-            <Link to={option.link}>{option.name}</Link>
-          </div>
+          <Link
+            className="h-full w-full p-1 duration-100 hover:bg-gray-300"
+            key={index}
+            to={option.link}>
+            {option.name}
+          </Link>
         ))}
       </div>
     </button>
