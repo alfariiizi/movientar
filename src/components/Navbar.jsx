d7351456f4699073e2727f129f4bbd1f2406578b
alfariiizi <alfariiiziiiii@gmail.com>
2023-02-24 22:39:14 +0700

    Changing background color on dropdown navbar'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,9 +13,11 @@ const Navbar = ({ titleSite }) => {
 
   return (
     <nav className="relative z-[100]">
-      <div className="fixed flex h-14 w-full items-center justify-between gap-2 border-b-[1px] border-b-slate-900 bg-slate-100/30 px-4 backdrop-blur-md">
-        <h1 className="flex-none text-center text-xl sm:w-1/3">{titleSite}</h1>
-        <div className="flex grow items-center justify-center gap-4">
+      <div className="fixed flex h-14 w-full items-center justify-between gap-2 border-b-[1px] border-b-slate-900 bg-slate-100/30 px-4 font-inter backdrop-blur-md">
+        <h1 className="flex-none text-center font-aboreto text-xl font-bold sm:w-1/3">
+          {titleSite}
+        </h1>
+        <div className="flex grow items-center justify-end gap-1 sm:mx-20 sm:gap-4">
           <Dropdown name={"Movies"} options={movieOptions} />
           <Dropdown name={"TV Series"} options={tvOptions} />
         </div>
--- src/tools/Dropdown.jsx
+++ src/tools/Dropdown.jsx
@@ -4,13 +4,13 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Dropdown = ({ name, options }) => {
   return (
-    <button className="group relative flex h-full items-center justify-between border-2 border-slate-800 bg-gray-200 p-2">
+    <button className="group relative flex h-full items-center justify-between border-2 border-slate-800 p-2">
       {name}
       <RiArrowDropDownLine />
-      <div className="absolute top-10 left-0 z-[110] hidden h-auto w-auto flex-col gap-1 border-2 border-slate-800 bg-gray-200 duration-100 group-hover:flex">
+      <div className="absolute top-10 left-0 z-[110] hidden h-auto w-auto flex-col divide-y-2 divide-slate-400 border-2 border-slate-800 duration-100 group-hover:flex">
         {options.map((option, index) => (
           <Link
-            className="h-full w-full p-1 duration-100 hover:bg-gray-300"
+            className="h-full w-full bg-slate-100/80 p-1 duration-100 hover:bg-slate-200"
             key={index}
             to={option.link}>
             {option.name}
