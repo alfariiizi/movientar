30c2e27b68e89c4d14d67dccfed8f3978dc8aea4
alfariiizi <alfariiiziiiii@gmail.com>
2023-02-24 21:03:15 +0700

    Adding beautiful animation when hover on cards

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -14,7 +14,9 @@ const List = ({ kind }) => {
       {/* List of all cards */}
       <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-4">
         {getList.map((movie, index) => (
-          <div key={index} className="group relative h-80 w-40 transition-all">
+          <div
+            key={index}
+            className="group relative top-0 left-0 h-80 w-40 transition-all duration-200 hover:-top-1 hover:-left-1">
             <div className="relative z-10 flex h-full w-full flex-col border-2 border-slate-800 bg-slate-200">
               {/* Card image */}
               <div className="h-60 w-full bg-blue-400">
@@ -32,7 +34,7 @@ const List = ({ kind }) => {
               </div>
             </div>
             {/* Backside retro card style */}
-            <div className="absolute top-2 left-2 h-full w-full border-2 border-slate-800 bg-teal-500 opacity-0 duration-200 group-hover:opacity-100"></div>
+            <div className="absolute top-0 left-0 h-full w-full border-2 border-slate-800 bg-teal-500 opacity-0 duration-200 group-hover:top-2 group-hover:left-2 group-hover:opacity-100"></div>
           </div>
         ))}
       </div>
