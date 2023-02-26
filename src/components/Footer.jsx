3de106e5de356b7a02bdd8c0ed96b7cc2312c1f8
alfariiizi <alfariiiziiiii@gmail.com>
2023-02-27 01:26:52 +0700

    Changing footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="bg-indigo-800 px-5 py-4 text-slate-200">
-        This data is gotten from{" "}
+        The data are gotten from{" "}
         <a
           href="https://www.themoviedb.org/"
           className="text-blue-400 hover:text-blue-500">
